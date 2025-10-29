6d0f2141b479ad17f725423e21016070756f0480
Annie Wang <aqw5781@psu.edu>
2025-10-29 13:30:00 -0400

    Deleted extraneous notebook, tested the shell concept (but not on an actual CAN network)

--- .ipynb_checkpoints/CanReader-checkpoint.py
+++ .ipynb_checkpoints/CanReader-checkpoint.py
@@ -1,18 +1,24 @@
 import can 
 import time
 class CanReader:
-    def __init__(self):
-        self.bus=can.interface.Bus(channel="can0", bustype="socketcan")
-        self.msg=self.bus.recv()
-        self.timestamp=time.strftime("%H: %M: %S", time.localtime())
-        self.arb_id=hex(msg.arbitration_id)
-        self.data_hex=msg.data.hex()
+    def __init__(self, msg):
+        self.msg=msg
+        #self.bus=can.interface.Bus(channel="can0", bustype="socketcan")
+        #self.msg=self.bus.recv()
+        #self.timestamp=time.strftime("%H: %M: %S", time.localtime())
+        #self.arb_id=hex(msg.arbitration_id)
+        #self.data_hex=msg.data.hex()
     def getTime(self):
-       return self.timestamp
+        timestamp=time.strftime("%H: %M: %S", time.localtime())
+        return timestamp
     def get_id(self):
-       return self.arb_id
+        arb_id=hex(self.msg.arbitration_id)
+        return arb_id
+       
     def get_hex(self):
-       return self.data_hex
+        data_hex=self.msg.data.hex()
+        return data_hex
+       
     def __str__(self):
         #print(f"[{timestamp}] ID={arb_id} Data={data_hex.upper()}")
-        return f"[{self.timesamp}] ID={self.arb_id} Data={self.data_hex.upper()}"
+        return f"[{self.getTime()}] ID={self.get_id()} Data={self.get_hex().upper()}"
