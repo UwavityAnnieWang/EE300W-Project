bb674f7a4caabd5f1b1855f99bc6a56ad9843666
Annie Wang <aqw5781@psu.edu>
2025-11-18 18:07:43 -0500

    Changed to QtWidget

--- CAN_BUS_GUI.py
+++ CAN_BUS_GUI.py
@@ -1,15 +1,16 @@
 import can
 import time
 import pyqtgraph as pg
+from PyQt6 import QtWidgets 
 from pyqtgraph.Qt import QtGui
 
-app = QtGui.QApplication([])
+app = QtWidgets.QApplication([])
 win = pg.GraphicsLayoutWidget(show=True, title="CAN Bus Live Data")
 plot = win.addPlot(title="Speed over Time")
 curve = plot.plot(pen='y')
 data_points = []
 
-bus = can.interface.Bus(channel="can0", bustype="socketcan")
+bus = can.interface.Bus(channel="can0", interface="socketcan")
 
 while True:
     msg = bus.recv()
